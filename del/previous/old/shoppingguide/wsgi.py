bd15e4b609072e1164b349e0af68f2f408584648
Abhishek <205119002@nitt.edu>
2020-11-26 15:46:47 +0530

    done

--- del/previous/old/shoppingguide/wsgi.py
+++ del/previous/old/shoppingguide/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shoppingGuide project.
+WSGI config for shoppingguide project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoppingGuide.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoppingguide.settings')
 
 application = get_wsgi_application()
