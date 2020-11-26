0d75fa32ba5fd740d3bd6197cacc928dd579efb6
Abhishek <205119002@nitt.edu>
2020-11-26 14:36:09 +0530

    basharat merged

---
+++ Amazon/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, HttpResponse
+import requests
+from bs4 import BeautifulSoup
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def product(request):
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+    print("hello")
+    if request.method == "POST":
+        searchItem = request.POST.get('searchbox')
+        searchItem = searchItem.replace(' ','+')
+        url = 'https://www.amazon.in/s?k=' + searchItem + '&rh=n%3A1389401031&ref=nb_sb_noss'
+        r = requests.get(url,headers=headers)
+        content = r.content
+        soup = BeautifulSoup(content,'html.parser')
+
+        image = soup.find(class_='a-section aok-relative s-image-fixed-height')
+        image = image.find('img')
+        image = image['src']
+
+        name = soup.find(class_='a-size-medium a-color-base a-text-normal').text #name
+        price = soup.find(class_='a-price-whole').text #price
+        reviews = soup.find(class_='a-icon a-icon-star-small a-star-small-4 aok-align-bottom').text #review points
+        people = soup.find(class_='a-row a-size-small') #people
+        peopleRated = people.find(class_='a-size-base').text #peopleRated
+        # //////////////////////////////////
+
+        url = 'https://www.flipkart.com/search?q='
+        query = request.POST.get('searchbox')
+        query = query.replace(' ','+')
+        url+=query
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content)
+
+        fname = None #name
+        if(soup.find(class_='_4rR01T')==None):
+            fname = soup.find(class_='s1Q9rs')
+        else:
+            fname = soup.find(class_='_4rR01T')
+        fprice = soup.find(class_='_30jeq3') #price
+        
+        freviews = soup.find(class_='_3LWZlK') #review points
+        frev = soup.select('span._2_R_DZ')
+        frev2 = frev[0].find_all('span')
+        fpeople_rated = frev2[1].text
+        fpeople_reviewed = frev2[3].text
+
+
+
+
+        
+    context = {
+        'price' : price,
+        'name' : name,
+        'reviews' : reviews,
+        'peopleRated' : peopleRated,
+        'image' : image,
+
+        'fname':fname.text,
+        'fprice':fprice.text,
+        'freviews':freviews.text,
+        'fpeople_rated':fpeople_rated,
+        'fpeople_reviewed':fpeople_reviewed,
+
+    }
+    return render(request, 'product.html', context)
--- old/shoppingguide/asgi.py
+++ old/shoppingguide/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for shoppingGuide project.
+ASGI config for shoppingguide project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoppingGuide.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoppingguide.settings')
 
 application = get_asgi_application()
